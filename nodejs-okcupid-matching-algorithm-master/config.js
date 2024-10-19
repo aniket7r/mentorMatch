929b4a75b5e1c51e1e633c97eec98abe09569743
Aniket Kumar <143780855+aniket7r@users.noreply.github.com>
2024-10-19 19:59:47 +0530

    Delete nodejs-okcupid-matching-algorithm-master directory

--- nodejs-okcupid-matching-algorithm-master/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  importanceLevelPoints: [0, 1, 10, 50, 250],
-  maximumAcceptableAnswers: 4,
-  profilesFilePath: 'resources/data/profiles.json',
-  writeToFolder: 'output/'
-}
-
-module.exports = config
